610c78c5d5c9693e681ee76d964e2c9015fa993a
Waffle552 <unknownsmartguy552@gmail.com>
2020-01-06 20:39:21 +0000

    loads videos from folder auto

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const port = 3000
 
 
 class stream {
-    constructor(path) {
+    constructor(path, id) {
         this.path = `videos/${path}`
         this.stat = fs.statSync(this.path)
         this.fileSize = this.stat.size
-        this.postingPath = '/' + Math.random().toString(36).substr(2, 9);
+        this.postingPath = '/' + id
     }
     poster(req, res, object) {
         
@@ -39,6 +39,7 @@ class stream {
                 }
                 res.writeHead(200, object.head)
                 fs.createReadStream(object.path).pipe(res)
+                console.log(`User accessed "${this.postingPath}"`)
 
             }
     }
@@ -51,10 +52,52 @@ class stream {
     }
 }
 
+const videosPath = './videos'
+const videoConfig = './videos.json'
 var initStreams = {}
 
+var videosDir = fs.readdirSync(videosPath)
+var streamslist = {}
 
-streams = [new stream('vid1Done.mp4'), new stream('Whenyorureallybadatr6.mp4')]
+//sees if the config file exsist
+if(fs.existsSync(videoConfig)){
+    let rawdata = fs.readFileSync(videoConfig)
+    let _videoConfig = JSON.parse(rawdata)
+
+    //checks if a video is missing from the config
+    for(i = 0; i < Object.keys(_videoConfig).length; i++){
+        for(x = 0; x < videosDir.length; x++){
+            if(_videoConfig[Object.keys(_videoConfig)[i]] == videosDir[0]){
+                videosDir.splice(0)
+            }
+        }
+    }
+    
+    //adds vidoe to config
+    for(i = 0; i < videosDir.length; i++){
+        _videoConfig[Math.random().toString(36).substr(2, 9)] = videosDir[i]
+    }
+    let data = JSON.stringify(_videoConfig, null, 2);
+    fs.writeFile(videoConfig, data, (err) => {
+        if (err) throw err;});
+    streamslist = _videoConfig
+} else {
+    //if the config does not exsist make a new one
+    let videoslist = {}
+    for(i = 0; i < videosDir.length; i++){
+        videoslist[Math.random().toString(36).substr(2, 9)] = videosDir[i]
+    }
+    let data = JSON.stringify(videoslist, null, 2);
+    fs.writeFile(videoConfig, data, (err) => {
+        if (err) throw err;});
+    streamslist = videoslist
+}
+
+var streams = []
+
+for(i = 0; i < Object.keys(streamslist).length; i++){
+    streams[i] = new stream(streamslist[Object.keys(streamslist)[i]], Object.keys(streamslist)[i]) 
+}
 
 for(i = 0; i < streams.length; i++){
     streams[i].post()
